38b2b105daf061d52350a99a0f7c8ac69d34ee9c
lmoreno127 <luismorenor127@hotmail.com>
2019-08-05 16:34:30 -0500

    Add Twitter App

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,29 @@
 class TweetsController < ApplicationController
 
   def index
+    @user=current_user
     @tweets = Tweet.all.order(created_at: :desc)
   end
 
   def new
+    @user=current_user
     @tweet = Tweet.new
   end
 
   def create
     tweet = Tweet.new(tweet_params)
-    if tweet.save
+    tweet.user=current_user
+    if tweet.save 
       redirect_to tweets_path
     else
       message =  {error: tweet.errors.full_messages.to_sentence}
       redirect_to new_tweet_path, flash: message
     end
   end
+
+  def show
+    @user=current_user
+  end  
 
   private
   def tweet_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_141011) do
+ActiveRecord::Schema.define(version: 2019_08_05_203411) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "body", limit: 140, default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "username", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
